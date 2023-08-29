e0a2ee431e85ca8ef5015183fc87a6493a8ffdc0
jessivanjunior <jessivanjunior@gmail.com>
2023-08-28 23:03:15 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:chat_firebase/shared/constants.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb){
+
+  }else{
+
+  }
+
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: Constants.apiKey,
+          appId: Constants.appId,
+          messagingSenderId: Constants.messagingSenderId,
+          projectId: Constants.projectId));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = '1:292539703424:web:736bccc8704a2d146ac665';
+  static String apiKey = 'AIzaSyCr-UvE6ngBMIDoDAj5Eqos1kG0QGJ6Yg0';
+  static String messagingSenderId = '292539703424';
+  static String projectId = 'flutterchat-7f080';
+}
